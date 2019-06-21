9f7c4a0c52751f22ceb1ee5aca60551a5c770e10
Charan-Venkatesan <charan.venkatesan94@gmail.com>
2019-06-20 19:16:05 -0500

    Version 1

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ driver= '{ODBC Driver 17 for SQL Server}'
 cnxn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password)
 
 @app.route('/')
-def hello_world():
+def home():
     cur = cnxn.cursor()
     cur.execute("select StateName from voting WHERE TotalPop between 2000 and 8000")
     get1 = cur.fetchall()
@@ -51,6 +51,33 @@ def options():
     for row in get:
         points.append([row[0],row[1]])
     return render_template("list.html", p=points)
+
+@app.route('/records1')
+def records1():
+    return render_template('records1.html')
+
+@app.route('/options1', methods=['POST', 'GET'])
+def options1():
+    p = int(request.form['p']) * 1000
+    rows = []
+    get = []
+    c = []
+    points = []
+    points.append(['Total Population','State Count'])
+    cur = cnxn.cursor()
+    cur.execute("select max(TotalPop) from voting")
+    maxPop = cur.fetchone();
+    print (maxPop)
+    i = 0
+    while(i < maxPop[0] ):
+        cur.execute("select count(StateName) from voting WHERE TotalPop between ? and ?",(i,i+(p)))
+        get = cur.fetchone();
+        key = str(i)+"-"+str(i+(p))
+        points.append([key, get[0]])
+        i = i+(p)
+        print(points)
+    return render_template("list1.html", p=points)
+
 
 if __name__ == '__main__':
     app.run()
